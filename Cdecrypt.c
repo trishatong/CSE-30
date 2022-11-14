646d8ab931d2f94045dbbda1dd9c45b3e5d5d6b0
keithmuller <66692457+keithmuller@users.noreply.github.com>
2022-11-13 20:56:07 -0800

    Initial version

---
+++ Cdecrypt.c
@@ -0,0 +1,17 @@
+// version 1.0 11/12/2022
+#include <stdio.h>
+#include "cipher.h"
+#ifndef MYDECRYPT_C
+TODO("using decrypt.S");
+#else
+TODO("using Cdecrypt.c");
+
+int
+decrypt(char *iobuf, char *bookbuf, int cnt)
+{
+
+    // your code here 
+
+    return cnt;
+}
+#endif
---
+++ Cencrypt.c
@@ -0,0 +1,17 @@
+// version 1.0 11/12/2022
+#include <stdio.h>
+#include "cipher.h"
+#ifndef MYENCRYPT_C
+TODO("using encrypt.S");
+#else
+TODO("using Cencrypt.c");
+
+int
+encrypt(char *iobuf, char *bookbuf, int cnt)
+{
+
+    // your code here
+
+    return cnt;
+}
+#endif
